6969346ea23d0c88e64857eb186042ab07b91985
OlgaMahryrtska <42007610+OlgaMahryrtska@users.noreply.github.com>
2021-01-05 12:19:41 +0100

    Create private-cookies.component.ts

---
+++ client/src/app/footer/private-cookies/private-cookies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-cookies',
+  templateUrl: './private-cookies.component.html',
+  styleUrls: ['./private-cookies.component.css']
+})
+export class PrivateCookiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
